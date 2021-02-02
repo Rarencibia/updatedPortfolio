e5429f3735afaef9cde0ab549d2249922e728eba
Rarencibia <40923510+Rarencibia@users.noreply.github.com>
2021-02-02 13:24:32 -0500

    fix nav bar with cancel button in mobile nav

--- javascript/dist/mobileNav.dev.js
+++ javascript/dist/mobileNav.dev.js
@@ -5,12 +5,17 @@ var _this = void 0;
 var mobileNav = document.getElementById('mobileNav');
 var mobileNavMenu = document.getElementById('mobileNavMenu');
 var mobileNavHamburger = document.getElementById('mobileNavHamburger');
-var mobileLI = document.getElementsByClassName("mobileLI"); //Open up mobile Navbar
+var mobileLI = document.getElementsByClassName("mobileLI");
+var cancelButton = document.getElementById("cancelButton"); //Open up mobile Navbar
 
 mobileNavHamburger.addEventListener("click", function () {
   _this.mobileNavHamburger.style.display = "none"; //When you click on the hamburger, it disappears.
 
   mobileNavMenu.style.display = "flex";
+});
+cancelButton.addEventListener("click", function () {
+  mobileNavMenu.style.display = "none";
+  mobileNavHamburger.style.display = "block";
 }); //Closing mobile Navbar
 
 for (var i = 0; i < mobileLI.length; i++) {
--- javascript/mobileNav.js
+++ javascript/mobileNav.js
@@ -2,12 +2,19 @@ let mobileNav = document.getElementById('mobileNav');
 let mobileNavMenu = document.getElementById('mobileNavMenu');
 let mobileNavHamburger = document.getElementById('mobileNavHamburger');
 let mobileLI = document.getElementsByClassName("mobileLI");
+let cancelButton = document.getElementById("cancelButton");
 
 //Open up mobile Navbar
 mobileNavHamburger.addEventListener("click", () => {
     this.mobileNavHamburger.style.display = "none"; //When you click on the hamburger, it disappears.
     mobileNavMenu.style.display = "flex"
 });
+
+cancelButton.addEventListener("click", () => {
+    mobileNavMenu.style.display = "none";
+    mobileNavHamburger.style.display = "block";
+})
+
 
 //Closing mobile Navbar
 for(let i = 0; i < mobileLI.length; i++){
