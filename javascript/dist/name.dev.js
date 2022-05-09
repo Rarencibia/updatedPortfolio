16d2bed5e4d72a75e6606a1c50b1908f0a2c4147
Rarencibia <40923510+Rarencibia@users.noreply.github.com>
2022-05-08 21:54:18 -0400

    new mobile event listener changes

--- javascript/dist/name.dev.js
+++ javascript/dist/name.dev.js
@@ -7,10 +7,21 @@ var timer = function timer(ms) {
   return new Promise(function (res) {
     return setTimeout(res, ms);
   });
-}; //transitionend ()
+}; //transitionend
+// document.getElementById("logo").addEventListener("load", 
+//     async function loadLetters () { // We need to wrap the loop into an async function for this to work
+//         for (var i = 0; i < myName.length; i++) {
+//             let letterContainer = document.createElement("div");
+//             letterContainer.setAttribute("class", "letterContainer");
+//             letterContainer.setAttribute("id", myName[i]);
+//             letterContainer.textContent = myName[i];
+//             nameContainer.appendChild(letterContainer);
+//             await timer(300); // then the created Promise can be awaited
+//         }
+//     });
 
 
-document.getElementById("logo").addEventListener("load", function loadLetters() {
+window.addEventListener("load", function loadLetters() {
   var i, letterContainer;
   return regeneratorRuntime.async(function loadLetters$(_context) {
     while (1) {
--- javascript/name.js
+++ javascript/name.js
@@ -4,27 +4,48 @@ let myName = ["A","R", "E", "N", "C","I", "B","I", "A"];
 
 
 const timer = ms => new Promise(res => setTimeout(res, ms));
-)
 
-document.getElementById("logo").addEventListener("load", //transitionend Was old way of getting the function to load after AOS flip of the avatar ended.
-    async function loadLetters () { // We need to wrap the loop into an async function for this to work
-        for (var i = 0; i < myName.length; i++) {
-            let letterContainer = document.createElement("div");
-            letterContainer.setAttribute("class", "letterContainer");
-            letterContainer.setAttribute("id", myName[i]);
-            letterContainer.textContent = myName[i];
+//transitionend
 
-            nameContainer.appendChild(letterContainer);
+// document.getElementById("logo").addEventListener("load", 
+//     async function loadLetters () { // We need to wrap the loop into an async function for this to work
+//         for (var i = 0; i < myName.length; i++) {
+//             let letterContainer = document.createElement("div");
+//             letterContainer.setAttribute("class", "letterContainer");
+//             letterContainer.setAttribute("id", myName[i]);
+//             letterContainer.textContent = myName[i];
+
+//             nameContainer.appendChild(letterContainer);
 
 
             
 
 
-            await timer(300); // then the created Promise can be awaited
+//             await timer(300); // then the created Promise can be awaited
 
 
-        }
+//         }
         
-    });
+//     });
 
 
+window.addEventListener("load", 
+async function loadLetters () { // We need to wrap the loop into an async function for this to work
+    for (var i = 0; i < myName.length; i++) {
+        let letterContainer = document.createElement("div");
+        letterContainer.setAttribute("class", "letterContainer");
+        letterContainer.setAttribute("id", myName[i]);
+        letterContainer.textContent = myName[i];
+
+        nameContainer.appendChild(letterContainer);
+
+
+        
+
+
+        await timer(300); // then the created Promise can be awaited
+
+
+    }
+    
+})
